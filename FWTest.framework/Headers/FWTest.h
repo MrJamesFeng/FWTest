0aaf53c368326910967b854d159e88be32ccc79d
丰静 <812463918@qq.com>
2023-09-13 14:40:04 +0800

    新增Test2

---
+++ FWTest.framework/Headers/FWTest.h
@@ -0,0 +1,18 @@
+//
+//  FWTest.h
+//  FWTest
+//
+//  Created by 丰静 on 2023/9/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FWTest.
+FOUNDATION_EXPORT double FWTestVersionNumber;
+
+//! Project version string for FWTest.
+FOUNDATION_EXPORT const unsigned char FWTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FWTest/PublicHeader.h>
+#import <FWTest/FJTest1.h>
+
